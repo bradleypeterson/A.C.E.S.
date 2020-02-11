e41d9b8e6134266ef7917102d2cab8b448529491
Joseph Hwang <josephhwang@mail.weber.edu>
2020-02-10 20:52:12 -0700

    1/2 done with layout

--- Electron/aces/index.js
+++ Electron/aces/index.js
@@ -1,37 +1,41 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let window = null
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({
+  window = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    fullscreenable: false,
     webPreferences: {
       nodeIntegration: true
     }
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  window.loadFile('index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  window.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  window.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
+    window = null
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
@@ -46,7 +50,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (window === null) {
     createWindow()
   }
 })
---
+++ Electron/aces/scripts/ui.js
@@ -0,0 +1,56 @@
+function toggleMenu(){
+    if ($("#content-nav").hasClass("hidden")){
+        $("#content-nav").animate({
+            width: 200
+        })
+        $("#content-main").animate({
+            paddingLeft: 200
+        })
+        $("#content-nav").removeClass("hidden");
+    }
+    else{
+        $("#content-nav").animate({
+            width: 0
+        })
+        $("#content-main").animate({
+            paddingLeft: 0
+        })
+        $("#content-nav").addClass("hidden");
+    }
+
+    closeSubnav();
+}
+
+function coursesNav(){
+    $("#page-subnav").load("components/courseNav.html");
+    $("#page-subnav").css("display", "block");
+    $("#page-subnav").animate({
+        width: 200
+    })
+    // $("body").on("click", function(e){
+    //     e.stopPropagation();
+    //     closeSubnav();
+    // })
+}
+
+function sectionsNav(){
+    $("#page-subnav").load("components/sectionsNav.html");
+    $("#page-subnav").animate({
+        width: 200
+    },{
+        complete: function(){
+            $(this).css("display", "block")
+        }
+    })
+}
+
+function closeSubnav(){
+    $("#page-subnav").animate({
+        width: 0
+    },{
+        complete: function(){
+            $(this).css("display", "none")
+        }
+    })
+    $(document).off("click");
+}
