383860122ea6cd55da0301dde50ff82cd9fc026a
Joseph Hwang <josephhwang@mail.weber.edu>
2020-03-25 21:29:12 -0600

    Updated gitignore

--- A.C.E.S. 2.0/ACES Dashboard/A.C.E.S/A.C.E.S/obj/Debug/netcoreapp3.1/Razor/Pages/Sections/Sections.cshtml.g.cs
+++ A.C.E.S. 2.0/ACES Dashboard/A.C.E.S/A.C.E.S/obj/Debug/netcoreapp3.1/Razor/Pages/Sections/Sections.cshtml.g.cs
@@ -13,7 +13,7 @@ namespace A.C.E.S.Pages.Sections
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
 #nullable restore
-#line 1 "S:\My Stuff\School\CS 4450\A.C.E.S.-2.0\A.C.E.S. 2.0\ACES Dashboard\A.C.E.S\A.C.E.S\Pages\_ViewImports.cshtml"
+#line 1 "D:\Documents\_School\2020_1_Spring\CS 4450 Software\A.C.E.S\A.C.E.S. 2.0\ACES Dashboard\A.C.E.S\A.C.E.S\Pages\_ViewImports.cshtml"
 using A.C.E.S;
 
 #line default
@@ -53,7 +53,7 @@ using A.C.E.S;
         {
 #nullable restore
 #line 3 "D:\Documents\_School\2020_1_Spring\CS 4450 Software\A.C.E.S\A.C.E.S. 2.0\ACES Dashboard\A.C.E.S\A.C.E.S\Pages\Sections\Sections.cshtml"
-
+  
     ViewData["Title"] = "Sections";
 
 #line default
