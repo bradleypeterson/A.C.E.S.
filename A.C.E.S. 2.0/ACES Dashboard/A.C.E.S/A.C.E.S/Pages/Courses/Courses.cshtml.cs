7d2afea4f0f5d6c70a30f7ae850760ef70efe57f
Robert Pickard <50324603+Extantdread@users.noreply.github.com>
2020-04-01 20:58:08 -0600

    Course Data from Database

--- A.C.E.S. 2.0/ACES Dashboard/A.C.E.S/A.C.E.S/Pages/Courses/Courses.cshtml.cs
+++ A.C.E.S. 2.0/ACES Dashboard/A.C.E.S/A.C.E.S/Pages/Courses/Courses.cshtml.cs
@@ -5,21 +5,28 @@ using System.Threading.Tasks;
 using A.C.E.S.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using A.C.E.S.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace A.C.E.S.Pages.Courses
 {
     public class CoursesModel : PageModel
     {
-        public List<Course> Courses { get; set; }
-        public void OnGet()
+        private readonly A.C.E.S.Data.ACESContext _context;
+
+        public CoursesModel(A.C.E.S.Data.ACESContext context)
         {
-            Courses = new List<Course>();
-            //Courses.Add(new Course(10000, "CS 1040"));
-            //Courses.Add(new Course(10001, "CS 1035"));
-            //Courses[1].Archived = true;
-            //Courses[0].Assignments.Add(new Assignment(1000, "Assignment 1"));
-            //Courses[0].Assignments.Add(new Assignment(1001, "Assignment 2"));
-            //Courses[0].Assignments.Add(new Assignment(1002, "Assignment 3"));
+            _context = context;
+        }
+
+        public IList<Course> Courses { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Courses = await _context.Courses
+                .Include(c => c.Assignments)
+                .AsNoTracking()
+                .ToListAsync();
         }
     }
 }
