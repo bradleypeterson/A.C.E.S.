692a08d746720a35d86dd701f8d185b9eb3d812b
Joseph Hwang <josephhwang@mail.weber.edu>
2020-04-06 20:29:12 -0600

    Fixed edit button

---
+++ A.C.E.S. 2.0/ACES Dashboard/A.C.E.S/A.C.E.S/Pages/Sections/Edit.cshtml.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using A.C.E.S.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace A.C.E.S.Pages.Sections
+{
+    public class EditModel : PageModel
+    {
+        private readonly A.C.E.S.Data.ACESContext _context;
+
+        public EditModel(A.C.E.S.Data.ACESContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Section Section { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Section = await _context.Sections.FindAsync(id);
+
+            if (Section == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sectionToUpdate = await _context.Sections.FindAsync(id);
+
+            if (sectionToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            if (await TryUpdateModelAsync<Section>(
+                 sectionToUpdate,
+                 "section",   // Prefix for form value.
+                   s => s.Name, s => s.Semester, s => s.Year, s => s.Length))
+            {
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Sections");
+            }
+
+            return Page();
+        }
+    }
+}
